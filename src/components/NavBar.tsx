1d37e576d7ab58a7814c73f3f4a0cdfbdd3757ba
Joseph <jbenson@softwareinstitute.com>
2025-03-12 14:47:21 +0000

    Added identifiers to the Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,11 +8,11 @@ export function NavBar({ onStateChange , isAdmin = false }: NavBarProps  ) {
     return (
         <nav className="navbar">
             <div className="nav-content">
-                <button className="nav-link" onClick={() => onStateChange(0)}>Home</button>
-                <button className="nav-link" onClick={() => onStateChange(1)}>Search</button>
-                <button className="nav-link" onClick={() => onStateChange(2)}>Settings</button>
-                {isAdmin && <button className="nav-link admin-only" onClick={() => onStateChange(4)}>Add Movie</button>}
-                <button className="nav-link" onClick={() => onStateChange(3)}>About</button>
+                <button id="Home" className="nav-link" onClick={() => onStateChange(0)}>Home</button>
+                <button id="Search" className="nav-link" onClick={() => onStateChange(1)}>Search</button>
+                <button id="Settings" className="nav-link" onClick={() => onStateChange(2)}>Settings</button>
+                {isAdmin && <button id="isAdmin" className="nav-link admin-only" onClick={() => onStateChange(4)}>Add Movie</button>}
+                <button id="About" className="nav-link" onClick={() => onStateChange(3)}>About</button>
             </div>
         </nav>
     )
